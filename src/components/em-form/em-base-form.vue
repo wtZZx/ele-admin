87ee0d884c359ad29a585647074b979c3d4498bc
wtzzx <wtzzx1007@gmail.com>
2018-03-20 10:10:10 +0800

    迁徙至github

--- src/components/em-form/em-base-form.vue
+++ src/components/em-form/em-base-form.vue
@@ -1,5 +1,64 @@
 <template>
-    <div class="em-box em-box-primary">
+    <div>
+        <div class="em-breadcrumb-wrap">
+            <el-breadcrumb separator-class="el-icon-arrow-right">
+                <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+                <el-breadcrumb-item>普通表单</el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <el-row>
+            <el-col :span="24">
+                <el-card class="form-wrap">
+                    <div slot="header" class="clearfix">
+                        <span>行内表单</span>
+                    </div>
+                    <div>
+                        <el-form :inline="true" :model="formInline" class="demo-form-inline">
+                            <el-form-item label="姓名">
+                                <el-input v-model="formInline.user" placeholder="请输入姓名"></el-input>
+                            </el-form-item>
+                            <el-form-item label="性别">
+                                <el-select v-model="formInline.sex" placeholder="请选择姓别">
+                                    <el-option label="男" value="male"></el-option>
+                                    <el-option label="女" value="female"></el-option>
+                                </el-select>
+                            </el-form-item>
+                            <el-form-item>
+                                <el-button type="primary">查询</el-button>
+                            </el-form-item>
+                        </el-form>
+                    </div>
+                </el-card>
+            </el-col>
+        </el-row>
+
+        <el-row>
+            <el-col :span="10">
+                <el-card class="form-wrap">
+                    <div slot="header" class="clearfix">
+                        <span>水平表单</span>
+                    </div>
+                </el-card>
+            </el-col>
+
+            <el-col :span="14">
+                <el-card class="form-wrap">
+                    <div slot="header" class="clearfix">
+                        <span>垂直表单</span>
+                    </div>
+                </el-card>
+            </el-col>
+        </el-row>
+
+        <el-row>
+            <el-col :span="12">
+                <el-card class="form-wrap">
+                    <div slot="header" class="clearfix">
+                        <span>带验证的表单</span>
+                    </div>
+                </el-card>
+            </el-col>
+        </el-row>
     </div>
 </template>
 <script>
@@ -7,7 +66,10 @@ export default {
     name: 'emBaseForm',
     data() {
         return {
-            activeName: "second"
+            formInline: {
+                user: '',
+                sex: ''
+            }
         };
     },
     
@@ -22,5 +84,9 @@ export default {
 <style lang="scss" scoped>
 .em-tab-table {
     padding: 0 20px 0 20px;
+}
+
+.form-wrap {
+    margin: .5rem;
 }
 </style>
--- src/components/em-table/em-base-tab-table.vue
+++ src/components/em-table/em-base-tab-table.vue
@@ -1,18 +1,48 @@
 <template>
     <div>
         <div class="em-breadcrumb-wrap">
-                <el-breadcrumb separator-class="el-icon-arrow-right">
-                    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-                    <el-breadcrumb-item>标签列表</el-breadcrumb-item>
-                </el-breadcrumb>
-            </div>
-        <div class="em-box em-box-primary">
-            
+            <el-breadcrumb separator-class="el-icon-arrow-right">
+                <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+                <el-breadcrumb-item>标签列表</el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <div class="em-box em-box-primary">    
             <el-tabs v-model="activeName" @tab-click="handleClick" class="em-tab-table">
-                <el-tab-pane label="格兰芬多" name="Gryffindor">格兰芬多</el-tab-pane>
-                <el-tab-pane label="斯莱特林" name="Slytherin">斯莱特林</el-tab-pane>
-                <el-tab-pane label="拉文克劳" name="Ravenclaw">拉文克劳</el-tab-pane>
-                <el-tab-pane label="赫奇帕奇" name="Hufflepuff">赫奇帕奇</el-tab-pane>
+                <el-tab-pane label="格兰芬多" name="Gryffindor">
+                    <div class="em-box-controls with-border">
+                        <el-form ref="form" :model="queryArg" :inline="true">
+                            <el-form-item>
+                                <el-input v-model="queryArg.name" placeholder="姓名"></el-input>
+                            </el-form-item>
+
+                            <el-form-item>
+                                <el-input v-model="queryArg.mobile" placeholder="手机号"></el-input>
+                            </el-form-item>
+
+                            <el-form-item>
+                                <el-button-group>
+                                    <el-button icon="ion-refresh"> 重置</el-button>
+                                    <el-button type="primary" icon="ion-search" @click="query"> 查询</el-button>
+                                </el-button-group>
+                            </el-form-item>
+                        </el-form>
+                    </div>
+                </el-tab-pane>
+                <el-tab-pane label="斯莱特林" name="Slytherin">
+                    <div class="em-box-controls with-border">
+                        <el-form ref="form" :inline="true">
+                            <el-form-item>
+                                <el-input placeholder="姓名"></el-input>
+                            </el-form-item>
+                        </el-form>
+                    </div>
+                </el-tab-pane>
+                <el-tab-pane label="拉文克劳" name="Ravenclaw">
+                    
+                </el-tab-pane>
+                <el-tab-pane label="赫奇帕奇" name="Hufflepuff">
+                    
+                </el-tab-pane>
             </el-tabs>
         </div>
     </div>
@@ -22,13 +52,20 @@ export default {
     name: 'emBaseTabTable',
     data() {
         return {
-            activeName: "second"
+            activeName: "second",
+            queryArg: {
+                name: '',
+                mobile: ''
+            }
         };
     },
     
     methods: {
         handleClick(tab, event) {
             console.log(tab, event);
+        },
+        query() {
+
         }
     }
 };
